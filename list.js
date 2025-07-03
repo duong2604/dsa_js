8454d575d8285812212c238480a9e6e96d1f0379
duongbui <duongbuibd@gmail.com>
2025-07-03 10:16:40 +0700

    start here

---
+++ list.js
@@ -0,0 +1,4 @@
+function list() {
+  console.log("This is the list function");
+}
+export { list };
---
+++ main.js
@@ -0,0 +1,7 @@
+import * as list from "./list.js";
+
+function main() {
+  list.list();
+}
+
+main();
